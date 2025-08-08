9468933709c0cbc56d3547d76fd9145ab1755d31
Calvin James Maximo <calvinjames.maximo@gcash.com>
2025-08-08 10:44:46 +0800

    feat: Exercise 1: The "New Way" - A Simple Lambda Expression

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,16 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.function.Predicate;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Exercise 1: The "New Way" - A Simple Lambda Expression
+        Predicate<String> isLong = s -> s.length() > 10;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        String str1 = "short";
+        String str2 = "This is a very long string";
+
+        System.out.println("Is '" + str1 + "' long? " + isLong.test(str1));
+        System.out.println("Is '" + str2 + "' long? " + isLong.test(str2));
     }
 }
